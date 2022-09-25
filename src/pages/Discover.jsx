fbb301ca87a83aa83c1a5563d90ddbe3db7e787b
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-25 13:03:15 +0530

    now result are logged in console log

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,13 @@
 import {Error, Loader, SongCard} from '../components';
 import {genres} from '../assets/constants'
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
 const Discover = () => {
+    const {data, isFetching, error} = useGetTopChartsQuery();
     const genreTitle='Pop';
+    
+    console.log(data);
+
     return(<div className='flex flex-col'>
         <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
             <h2 className='font-bold text-3xl text-white text-left'>
@@ -14,7 +20,8 @@ const Discover = () => {
         </div>
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
             {[1,2,3,4,5,6,7,8,9,10].map((song, i)=>{
-                return <SongCard key={song.key} song={song} i={i}/>
+                
+                return <SongCard key={song} song={song} i={i}/>
             })}
         </div>
     </div>)
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", "7c58128526msh52d12bb929bf145p1b815fjsnfc6790c75c8d");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
