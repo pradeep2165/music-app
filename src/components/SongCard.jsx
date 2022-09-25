2debdda3a5102dc34b251eb7b81665782a27650b
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-25 14:37:25 +0530

    pause play component styled

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import PlayPause from "./PlayPause";
 
-const SongCard = ({ song }) => {
-  const activeSong = "test";
-
+const SongCard = ({ song, isPlaying, activeSong }) => {
+  const handlePauseClick = () => {};
+  const handlePlayClick = () => {};
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? "flex bg-opacity-70" : "hidden"}`}>
-          <PlayPause />
+          <PlayPause song={song} handlePause={handlePauseClick} handlePlay={handlePlayClick} isPlaying={isPlaying} activeSong={activeSong} />
         </div>
         <img src={song.images?.coverart} alt="song_img" />
       </div>
