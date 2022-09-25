8b9ce50f3b813c3226c1f665b238f6007e1dae1c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-26 02:56:50 +0530

    stace managed

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -19,6 +19,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
           <p className="text-base text-gray-400 mt-2">{artistId ? artist?.genreNames[0] : songData?.genres?.primary}</p>
         </div>
       </div>
+      <div className="w-full sm:h-44 h-24" />
     </div>
   );
 };
