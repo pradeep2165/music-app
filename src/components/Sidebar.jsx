d99d35b3f2361ac6136b692942044cb6b3f65f9c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-25 16:09:51 +0530

    mobile menu button working

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
 import { logo } from "../assets";
 import { links } from "../assets/constants";
-
+import { HiOutlineMenu } from "react-icons/hi";
 const NavLinks = ({ handleClick }) => {
   return (
     <div className="mt-10">
@@ -20,7 +20,7 @@ const NavLinks = ({ handleClick }) => {
 };
 
 const Sidebar = () => {
-  const [mobileMenuOpen, SetMobileMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <>
       <div
@@ -29,6 +29,17 @@ const Sidebar = () => {
       >
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
+      </div>
+
+      <div className="absolute md:hidden block top-6 right-3">{mobileMenuOpen ? <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} /> : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />}</div>
+
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gardient-to-tl from-white/10 
+      to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition 
+      ${mobileMenuOpen ? "left-0" : "-left-full"}`}
+      >
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
