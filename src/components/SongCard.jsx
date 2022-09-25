a77bbbd1f1d5fd620968bccef68ee1ca7702c1eb
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-25 14:50:12 +0530

    now music player is working

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,9 +1,18 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, isPlaying, activeSong }) => {
-  const handlePauseClick = () => {};
-  const handlePlayClick = () => {};
+const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
