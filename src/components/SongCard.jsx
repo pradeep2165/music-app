a845c16e041934a3044fb80c2e6501dfbc584015
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-25 13:43:10 +0530

    cover image tracked

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,22 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import PlayPause from "./PlayPause";
+
+const SongCard = ({song}) => {
+const activeSong = 'test';
+
+
+return(
+  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="relative w-full h-56 group">
+      <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-opacity-70' : 'hidden'}`}>
+      <PlayPause/>
+      </div> 
+      <img src={song.images?.coverart} alt="song_img" />
+      </div> 
+      
+      </div>
+)
+}
+
+
 
 export default SongCard;
